7771249e0a3ae4b150020e04fc9157ff37b0af09
Alona Husak <3900441@gmail.com>
2026-04-03 23:01:45 +0300

    Added new branch with folder and files with HW of Objects in JS

---
+++ js_objects/task1.js
@@ -0,0 +1,8 @@
+const student = {
+    firstName: "Карпо",
+    lastName: "Кайдаш",
+    age: 18,
+    courses: ["Математика", "Історія", "Програмування"]
+};
+
+console.log(student);
---
+++ js_objects/task2.js
@@ -0,0 +1,10 @@
+const book = {
+    title: "Кайдашева сім'я",
+    author: "Іван Семенович Нечуй-Левицький",
+    year: "1878"
+};
+
+const {title, author} = book;
+
+console.log(title);
+console.log(author);
